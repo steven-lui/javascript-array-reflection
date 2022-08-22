1e36602720b39d74b0d8afd1be12b36c9124ff89
steven-lui <steven.lui@netmatters-scs.com>
2022-08-22 11:29:02 +0100

    Add author
    Author is added to caption.
    Image is in original res

--- src/assets/js/xml.js
+++ src/assets/js/xml.js
@@ -1,22 +1,31 @@
-let idMax = 1082; //check for higher IDs
+let idMax = 1082; //  check for higher IDs
+let htmlTag = '#picsum-img'; //  for jQuery
 
 const getRandomID = function () { return Math.floor(Math.random() * idMax) + 1; }
 
 function setImage(xml) {
+    // parse
     let listObject = JSON.parse(xml.responseText);
-    $('#picsum-img').attr('src', `https://picsum.photos/id/${listObject.id}/300/300`);
+
+    // set HTML
+    $(htmlTag).attr('src', `https://picsum.photos/id/${listObject.id}/300/300`);
+    $(`${htmlTag}-caption`).text(`Image by ${listObject.author}`);
 }
 
 function getRandomImage() {
+    // init
     let xml = new XMLHttpRequest();
 
+    // assign
     xml.onreadystatechange = function () {
         //good to go
         if (xml.readyState === 4 && xml.status === 200) {
             setImage(this);
         }
+        //on a bad call, call itself
     };
 
+    // call
     xml.open('GET', `https://picsum.photos/id/${getRandomID()}/info`);
     xml.send();
 };
