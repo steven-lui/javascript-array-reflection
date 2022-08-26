f74d6eb694f3f72ea28fb3da8a828d3382bc79a6
steven-lui <steven.lui@netmatters-scs.com>
2022-08-26 10:20:56 +0100

    rename dist to docs

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,114 @@
 'use strict';
 
-import { MailImg } from './MailImg.js';
-import { ajaxToImg, randomImg } from './xhr.js';
+class MailImg {
+    constructor(mailTo = "", id = "") {
+        this._mailTo = mailTo;
+        this._idList = [id];
+    }
+
+    //get
+    get mailTo() {
+        return this._mailTo;
+    }
+
+    get idList() {
+        return this._idList;
+    }
+
+    //set
+    set mailTo(email) {
+        this._mailTo = email;
+    }
+
+    set idList(ids) {
+        this._idList = ids;
+    }
+
+    //newest first
+    addId = id => this._idList.push(id);
+
+    removeiD = id => {
+        // take the index of url
+        // splice it out of list
+        this._idList.splice(this.idList.indexOf(id), 1);
+    }
+}
+
+// https://www.taniarascia.com/how-to-promisify-an-ajax-call/
+function ajaxRequest(type, url, data = {}) {
+    return new Promise((resolve, reject) => {
+        $.ajax({
+            url: url,
+            type: type,
+            data: data,
+            success: function (data) {
+                resolve(data);
+            },
+            error: function (data) {
+                reject(data);
+            }
+        })
+    });
+}
+
+function applyDataToImg(data, htmlId, width, height) {
+    $(htmlId).attr('src', `https://picsum.photos/id/${data.id}/${width}/${height}`);
+    $(htmlId).text('alt', `Image by ${data.author}`);
+}
+
+// tested, it seems like max is 1082
+let maxId = 1082;
+const getRandomID = () => Math.floor(Math.random() * maxId) + 1;
+
+/* creates ajax request to get an image from lorem picsum */
+function ajaxToImg(imgId, htmlId, width, height) {
+    ajaxRequest('GET', `https://picsum.photos/id/${imgId}/info`)
+        .then(data => {
+            applyDataToImg(data, htmlId, width, height);
+        })
+        .catch(error => {
+            console.log("Error:", error.responseText);
+        });
+};
+
+// https://www.codingem.com/javascript-promises-in-for-loops/
+async function randomImg() {
+    let found = false;
+    let exData; //holder for request data
+    let maxAttempts = 5;
+
+    // attempt
+    for (let i = 0; i < maxAttempts; ++i) {
+        console.log(`Attempt ${i + 1} for randomImg()`);
+
+        let id = getRandomID();
+
+        // request a random img
+        await ajaxRequest('GET', `https://picsum.photos/id/${id}/info`)
+            // success
+            .then(data => {
+                exData = data;
+                console.log("Image found:", data.id);
+                found = true;
+            })
+            //error
+            .catch(data => {
+                console.log(`Error for ID#${id}: ${data.responseText}`);
+            });
+        // stop loop on success
+        if (found) {
+            applyDataToImg(exData, "#picsum-img", 400, 400);
+            break;
+        }
+
+    }
+
+    // fail
+    if (!found) {
+        $("#picsum-img").attr('src', './assets/img/not-found.jpg');
+        console.log("Too many attempts, please try again");
+    }
+}
 
 let mailList = []; //list of MailImg
 
